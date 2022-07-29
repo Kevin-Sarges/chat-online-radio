50e3fbcdec807aebdcf4b3508124d4b53917516c
Kevin-Sarges <kevinsarges08@gmail.com>
2022-07-29 14:44:26 -0300

    Configuração com google e firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:desafio_radio/app/app_widget.dart';
+import 'package:desafio_radio/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
